f25826f5196f338dd0d923fe6881d81f9e490a79
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-02-19 22:01:46 +0800

    Create arm-lede.sh

---
+++ arm-lede.sh
@@ -0,0 +1,85 @@
+rm -rf ./feeds/packages/net/chinadns-ng
+rm -rf ./feeds/packages/net/dns2socks
+#!/bin/bash
+svn_export() {
+	# 参数1是分支名, 参数2是子目录, 参数3是目标目录, 参数4仓库地址
+ 	echo -e "clone $4/$2 to $3"
+	TMP_DIR="$(mktemp -d)" || exit 1
+ 	ORI_DIR="$PWD"
+	[ -d "$3" ] || mkdir -p "$3"
+	TGT_DIR="$(cd "$3"; pwd)"
+	git clone --depth 1 -b "$1" "$4" "$TMP_DIR" >/dev/null 2>&1 && \
+	cd "$TMP_DIR/$2" && rm -rf .git >/dev/null 2>&1 && \
+	cp -af . "$TGT_DIR/" && cd "$ORI_DIR"
+	rm -rf "$TMP_DIR"
+}
+rm -rf ./feeds/luci/applications/luci-app-openclash
+rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/luci/applications/luci-app-ssr-plus
+rm -rf ./feeds/packages/net/dns2tcp
+rm -rf ./feeds/packages/net/hysteria
+rm -rf ./feeds/packages/net/ipt2socks
+rm -rf ./feeds/packages/lang/lua-neturl
+rm -rf ./feeds/packages/net/microsocks
+rm -rf ./feeds/packages/net/mosdns
+rm -rf ./feeds/packages/net/naiveproxy
+rm -rf ./feeds/packages/net/redsocks2
+rm -rf ./feeds/packages/net/shadowsocks-rust
+rm -rf ./feeds/packages/net/shadowsocksr-libev
+rm -rf ./feeds/packages/net/simple-obfs
+rm -rf ./feeds/packages/net/tcping
+rm -rf ./feeds/packages/net/trojan-go
+rm -rf ./feeds/packages/net/trojan
+rm -rf ./feeds/packages/net/trojan-plus
+rm -rf ./feeds/luci/applications/luci-app-trojan-server
+rm -rf ./feeds/packages/net/v2ray-core
+rm -rf ./feeds/packages/net/v2ray-plugin
+rm -rf ./feeds/packages/net/v2raya
+rm -rf ./feeds/packages/net/xray-core
+rm -rf ./feeds/packages/net/xray-plugin
+rm -rf ./feeds/packages/net/pdnsd-alt
+rm -rf ./feeds/packages/net/trojan-go
+rm -rf ./feeds/packages/net/trojan-plus
+rm -rf ./feeds/packages/net/v2ray-geodata
+rm -rf ./feeds/packages/lang/golang
+
+git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+git clone --depth 1 https://github.com/sbwml/luci-app-xunlei  package/luci-app-xunlei
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone --depth 1 https://github.com/fw876/helloworld package/helloworld
+git clone --depth 1 https://github.com/chenmozhijin/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth 1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+svn_export "main" "luci-app-passwall2" "package/luci-app-passwall2" "https://github.com/xiaorouji/openwrt-passwall2"
+svn_export "main" "luci-app-passwall" "package/luci-app-passwall" "https://github.com/xiaorouji/openwrt-passwall"
+svn_export "main" "luci-app-amlogic" "package/luci-app-amlogic" "https://github.com/ophub/luci-app-amlogic"
+svn_export "dev" "luci-app-openclash" "package/luci-app-openclash" "https://github.com/vernesong/OpenClash"
+svn_export "v5" "luci-app-mosdns" "package/luci-app-mosdns" "https://github.com/sbwml/luci-app-mosdns"
+svn_export "v5" "mosdns" "package/mosdns" "https://github.com/sbwml/luci-app-mosdns"
+svn_export "v5" "v2dat" "package/v2dat" "https://github.com/sbwml/luci-app-mosdns"
+svn_export "master" "luci-app-netspeedtest" "package/luci-app-netspeedtest" "https://github.com/sirpdboy/netspeedtest"
+svn_export "master" "homebox" "package/homebox" "https://github.com/sirpdboy/netspeedtest"
+cd package
+# 个性化设置
+sed -i "s|amlogic_firmware_repo.*|amlogic_firmware_repo 'https://github.com/OldCoding/openwrt_packit_arm'|g" luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|ARMv8|ARMv8-im|g" luci-app-amlogic/root/etc/config/amlogic
+rm -rf luci-app-netspeedtest/po/zh_Hans
+# 汉化
+curl -sfL -o ./convert_translation.sh https://github.com/kenzok8/small-package/raw/main/.github/diy/convert_translation.sh
+chmod +x ./convert_translation.sh && bash ./convert_translation.sh
+# OpenClash
+cd ./luci-app-openclash/root/etc/openclash
+CORE_VER=https://github.com/vernesong/OpenClash/raw/core/dev/core_version
+CORE_TUN=https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-arm64
+CORE_DEV=https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-arm64.tar.gz
+CORE_MATE=https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-arm64.tar.gz
+TUN_VER=$(curl -sfL $CORE_VER | sed -n "2{s/\r$//;p;q}")
+curl -sfL -o ./Country.mmdb https://github.com/alecthw/mmdb_china_ip_list/raw/release/Country.mmdb
+curl -sfL -o ./GeoSite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
+curl -sfL -o ./GeoIP.dat https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+mkdir ./core && cd ./core
+curl -sfL -o ./tun.gz "$CORE_TUN"-"$TUN_VER".gz && gzip -d ./tun.gz && mv ./tun ./clash_tun
+curl -sfL -o ./meta.tar.gz "$CORE_MATE" && tar -zxf ./meta.tar.gz && mv ./clash ./clash_meta
+curl -sfL -o ./dev.tar.gz "$CORE_DEV" && tar -zxf ./dev.tar.gz
+chmod +x ./clash* ; rm -rf ./*.gz
